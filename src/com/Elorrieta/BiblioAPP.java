d56ad561818d186bd039d8c9d3573563f1cb2cac
OPE <sanchis@martinvazquez.chendo>
2019-12-05 16:19:53 +0100

    INICIO

---
+++ src/com/Elorrieta/BiblioAPP.java
@@ -0,0 +1,10 @@
+package com.Elorrieta;
+
+public class BiblioAPP {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Inicio de proyecto");
+	}
+
+}
