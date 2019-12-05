ab91eba68ed795ccbc6767dba8cd6f8410cf9a5e
OPE <sanchis@martinvazquez.chendo>
2019-12-05 17:08:10 +0100

    MENU PRINCIPAL BASE HECHA, FALTA RELLENAR SWITCH

--- src/com/Elorrieta/BiblioAPP.java
+++ src/com/Elorrieta/BiblioAPP.java
@@ -1,10 +1,36 @@
 package com.Elorrieta;
 
+import java.util.Scanner;
+
 public class BiblioAPP {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inicio de proyecto");
+		System.out.println("por favor, introduce la operación");
+		System.out.println("A: Sacar libro");
+		System.out.println("B: Devolver libro");
+		System.out.println("S: Salir");
+		System.out.println("Seleccione opción");
+		
+		Scanner sc = new Scanner (System.in);
+		String opcion = sc.nextLine();
+		
+		switch (opcion) {
+		case "A":
+			
+			break;
+		case "B":
+			
+			break;
+		case "S":
+	
+			break;
+
+		default:
+			break;
+		}
+		
 	}
 
 }
